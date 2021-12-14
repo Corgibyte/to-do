f148fb6c8c56fabc7d8cbc7ab512455b2dfff67d
Hannah Young <hannah@corgibyte.com>
2021-12-13 22:10:04 -0800

    Implement homework through adding HttpGets and HttpPosts

---
+++ ToDoList/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+  public class HomeController : Controller
+  {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      Item starterItem = new Item("Add first item to ToDoList!");
+      return View(starterItem);
+    }
+
+    [HttpGet("/items/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/items")]
+    public ActionResult Create(string description)
+    {
+      Item myItem = new Item(description);
+      return View("Index", myItem);
+    }
+  }
+}
